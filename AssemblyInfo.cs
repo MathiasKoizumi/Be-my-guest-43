951e467fa9b9558aa7d1d7557daa4a11840787a4
MathiasKoizumi <mathiaskoizumi@hotmail.com>
2019-02-25 23:33:31 +0100

    Go

---
+++ AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿// Assembly WindowsFormsApplication1, Version 1.0.0.0
+
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Reflection.AssemblyProduct("WindowsFormsApplication1")]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9 Hewlett-Packard 2012")]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: System.Reflection.AssemblyTitle("WindowsFormsApplication1")]
+[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyCompany("Hewlett-Packard")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("fe8fdf15-34c4-4774-aeec-6588d4897e16")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+
---
+++ MP3Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public class MP3Player
+{
+    private string _command;
+    private bool isOpen;
+
+    public void Close()
+    {
+        this._command = "close MediaFile";
+        mciSendString(this._command, null, 0, IntPtr.Zero);
+        this.isOpen = false;
+    }
+
+    [DllImport("winmm.dll")]
+    private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+    public void Open(string sFileName)
+    {
+        this._command = "open \"" + sFileName + "\" type mpegvideo alias MediaFile";
+        mciSendString(this._command, null, 0, IntPtr.Zero);
+        this.isOpen = true;
+    }
+
+    public void Play(bool loop)
+    {
+        if (this.isOpen)
+        {
+            this._command = "play MediaFile";
+            if (loop)
+            {
+                this._command = this._command + " REPEAT";
+            }
+            mciSendString(this._command, null, 0, IntPtr.Zero);
+        }
+    }
+}
+
